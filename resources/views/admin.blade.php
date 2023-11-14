f497584c79eda71da553bbaebf5523f3b114f7d2
Rina Dwi <rinadwi@MacBook-Pro-Rina.local>
2023-11-14 20:55:32 +0700

    fitur cari

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -34,8 +34,7 @@
                     <a href="https://flowbite.com" class="flex ms-2 md:me-24">
                         <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 me-3" alt="FlowBite Logo" />
                         <span
-                            class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Arsip
-                            Desa Panjerejo</span>
+                            class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Arsip Mahasiswa</span>
                     </a>
                 </div>
                 <div class="flex items-center">
